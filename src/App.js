fc270cc6b61582f365a19529498a31f95210f635
Avery Wolf <averyw2@uw.edu>
2020-01-17 09:04:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+
+    <header>  
+      <img className='logo' 
+        alt='logo'
+        src='https://www.pinclipart.com/picdir/big/369-3696201_chat-group-conversation-svg-png-icon-free-.png'
+      />
+      Chatter
+    </header>
+
+    <TextInput onSend={t => console.log(t)}/>
+    
+  </main>
+}
+
+function Message() {
+  return <div className='message'>
+    <header> hey how is it going? </header>
+  </div> 
+}
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+  
+
+  return <div className='text-input'>
+    <input className='input'
+      value={text}
+      onChange={e=> setText(e.target.value)}
+    />
+    <button className='button'
+      onClick={() => {
+      props.onSend(text) 
+      setText('')
+      }}> 
+      <img className='sendbutton'
+        src='https://freepngimg.com/download/web_design/24677-8-up-arrow-transparent-image.png'
+      />
+    </button>
+  </div>
 }
 
 export default App;
